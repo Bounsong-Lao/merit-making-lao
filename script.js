271b0145e0f68a664fb3913afa0401fdc47b51d9
Bounsong-Lao <song123jk@gmail.com>
2025-06-06 09:47:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Smooth scrolling for navigation links
+    document.querySelectorAll('nav ul li a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // You can add more JavaScript functionality here later if needed.
+});
